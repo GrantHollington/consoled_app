62ba1ad14363b804f7890548e9f340aa40f943a4
GrantHollington <grant.j.hollington@gmail.com>
2021-08-03 13:43:31 +1000

    add conditional routing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
+  # before_action :authenticate_user!
 
   # GET /listings or /listings.json
   def index
@@ -12,7 +13,18 @@ class ListingsController < ApplicationController
 
   # GET /listings/new
   def new
-    @listing = Listing.new
+    # if the user not logged in redirect to the login page
+    # else redirect to the to the console/game form page
+    if user_signed_in?
+      # if user has created a profile show the console/game form else redirect to create a profile page
+      if current_user.profile
+        @listing = Listing.new
+      else 
+        redirect_to new_profile_path
+      end   
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   # GET /listings/1/edit
@@ -22,6 +34,8 @@ class ListingsController < ApplicationController
   # POST /listings or /listings.json
   def create
     @listing = Listing.new(listing_params)
+    #associate console listed to a seller
+    @listing.seller_id = current_user.profile.id
 
     respond_to do |format|
       if @listing.save
