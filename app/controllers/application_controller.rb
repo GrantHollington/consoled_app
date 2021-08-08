04202e11e12dd286fc8b26ab23cc4dbe13eff750
GrantHollington <grant.j.hollington@gmail.com>
2021-08-08 12:01:57 +1000

    add buyer worflow to app register to buy and params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,25 @@
 class ApplicationController < ActionController::Base
+
+    #capture the query param to be passed to the view
+    before_action :set_user_type
+
+    def set_user_type
+        @user_type = params[:user_type]
+    end
+
     #devise by deafault redirects to homepage after login. if we need to redirect anywhere else we need to set that up in the after_sign_in method (create a custom redirect hook)
     def after_sign_in_path_for(profile)
+
+        #if the user is a buyer and if they have a profile take them to the root path
         if(current_user.profile)
-            new_listing_path
+            if(params[:user][:user_type] == "buyer")
+                root_path
+            else
+                new_listing_path
+            end
         else
-            new_profile_path || root_path
+            #pass the user type while creating the new profile
+            new_profile_path(user_type: params[:user][:user_type]) || root_path
         end
     end
 end
